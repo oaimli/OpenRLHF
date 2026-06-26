308fba312618ed97c6c0965c2ee5d578e1f56b15
oaimli <miaoli.cs@gmail.com>
2026-06-26 12:15:26 +0100

    debug

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -362,10 +362,12 @@ class ActorPPOTrainer(ABC):
             **mm_inputs,
         )
         self.actor.gradient_checkpointing_disable()
-        print("output_logits", output["logits"].shape) # batch-size, sequence-len, vocab
-        print("output_full_logits", output_full["logits"].shape) # batch-size, sequence-len, vocab
-        output_logits = output["logits"][target_index: target_index + 1].detach()[action_mask[target_index: target_index + 1] == 1]
-        output_full_logits = output_full["logits"][action_mask_full == 1]
+        output_logits = output["logits"][:, :-1, :, :]
+        output_full_logits = output_full["logits"][:, :-1, :, :]
+        print("output_logits", output_logits.shape) # batch-size, sequence-len, 1, vocab
+        print("output_full_logits", output_full_logits.shape) # batch-size, sequence-len, 1, vocab
+        output_logits = output_logits[target_index: target_index + 1].detach()[action_mask[target_index: target_index + 1] == 1]
+        output_full_logits = output_full_logits[action_mask_full == 1]
         kl_per_token = F.kl_div(
             F.log_softmax(output_full_logits, dim=-1),
             F.log_softmax(output_logits, dim=-1),
