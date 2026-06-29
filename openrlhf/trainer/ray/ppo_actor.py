96d4a08ba36fc72394af46b2ee67ded7a7920429
oaimli <miaoli.cs@gmail.com>
2026-06-29 22:54:23 +0100

    when making experience, padding needs to consider sequences_full

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -260,7 +260,7 @@ class ActorPPOTrainer(ABC):
         old_action_log_probs = experience.action_log_probs
         advantages = experience.advantages
         base_action_log_probs = experience.base_action_log_probs
-        print("advantages", advantages.shape) # (batch-size, )
+        # print("advantages", advantages.shape) # (batch-size, sequence-len - 1)
 
         # VLM: merge pre-processed multimodal inputs for training forward
         mm_inputs = {}
@@ -340,10 +340,10 @@ class ActorPPOTrainer(ABC):
             return_entropy=self.args.actor.entropy_coef is not None,
             **mm_inputs,
         )
-        print("action_log_probs", action_log_probs.shape) # (batch-size, sequence-len - 1)
-        print("action_log_probs_full", action_log_probs_full.shape) # (batch-size, sequence-len - 1)
-        print("action_mask", action_mask.shape) # (batch-size, sequence-len - 1)
-        print("action_mask_full", action_mask_full.shape) # (batch-size, sequence-len - 1)
+        # print("action_log_probs", action_log_probs.shape) # (batch-size, sequence-len - 1)
+        # print("action_log_probs_full", action_log_probs_full.shape) # (batch-size, sequence-len - 1)
+        # print("action_mask", action_mask.shape) # (batch-size, sequence-len - 1)
+        # print("action_mask_full", action_mask_full.shape) # (batch-size, sequence-len - 1)
         log_ratio = action_log_probs[target_index: target_index + 1].detach()[action_mask[target_index: target_index + 1] == 1] - action_log_probs_full[action_mask_full == 1]
         kd_loss = log_ratio.mean()
         experience.info["kd_loss"] = kd_loss.detach()
