bad3ece86dd90efe672a07d9877b5df9cb66185e
oaimli <miaoli.cs@gmail.com>
2026-06-29 10:08:28 +0100

    debug

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -271,6 +271,7 @@ class ActorPPOTrainer(ABC):
             mm_inputs = merge_mm_train_inputs(experience.mm_train_inputs, sequences.device)
 
         # actor loss on proxy contexts, no ring attention, no gradient checkpointing
+        self.actor.gradient_checkpointing_disable()
         action_log_probs, output = self.actor(
             sequences,
             action_mask,
@@ -282,6 +283,7 @@ class ActorPPOTrainer(ABC):
             return_entropy=self.args.actor.entropy_coef is not None,
             **mm_inputs,
         )
+        self.actor.gradient_checkpointing_disable()
 
         # loss function
         actor_loss, clip_ratio, ppo_kl, vllm_kl = self.actor_loss_fn(
